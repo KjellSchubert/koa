b75ae0335f9b5f04fd25f49d1fb923d55c66b2c4
Kjell Schubert <kjell.schubert@gmail.com>
2014-10-26 17:20:22 -0400

    files needed for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'koa',
+  packages = ['koa'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Koa.js web framework port for asyncio + aiohttp',
+  author = 'Kjell Schubert',
+  author_email = 'kjell.schubert@gmail.com',
+  url = 'https://github.com/KjellSchubert/koa', # use the URL to the github repo
+  download_url = 'https://github.com/KjellSchubert/koa/tarball/0.1',
+  keywords = ['koa', 'koa.js', 'koajs', 'aiohttp', 'asyncio'],
+  classifiers = [],
+)
