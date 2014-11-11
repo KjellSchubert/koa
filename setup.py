8f4125f4527fbfd07f1b3d1f7fa7beed2af9e0ab
Kjell Schubert <kjell.schubert@gmail.com>
2014-11-11 11:12:20 -0500

    setup.py for 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'koa',
   packages = ['koa'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Koa.js web framework port for asyncio + aiohttp',
   author = 'Kjell Schubert',
   author_email = 'kjell.schubert@gmail.com',
   url = 'https://github.com/KjellSchubert/koa', # use the URL to the github repo
-  download_url = 'https://github.com/KjellSchubert/koa/tarball/0.2',
+  download_url = 'https://github.com/KjellSchubert/koa/tarball/0.3',
   keywords = ['koa', 'koa.js', 'koajs', 'aiohttp', 'asyncio'],
   classifiers = [],
 )
